7e81963574585b71014e1d149c655a19447b0e14
manish.sharma <manish.sharma@technizerindia.com>
2023-07-25 16:56:53 +0530

    Assignment Solution

--- Day2/1_ts-demos/9_assignment.ts
+++ Day2/1_ts-demos/9_assignment.ts
@@ -1,10 +1,48 @@
+// // Create a function Reverse which can accept string or array of any type and give the following output
+// function Reverse(word: string): string[];
+// function Reverse(words: string[]): string[];
+// function Reverse(word: number[]): number[];
+
+// // function Reverse(strOrArr: any) {
+// //     if (typeof strOrArr === "string") {
+// //         return strOrArr.split("").reverse();
+// //     } else {
+// //         return strOrArr.reverse();
+// //     }
+// // }
+
+// function Reverse(strOrArr: any) {
+//     if (typeof strOrArr === "string") {
+//         return strOrArr.split("").reverse();
+//     } else {
+//         return (Array<any>(strOrArr)).reverse();
+//     }
+// }
+
+// console.log(Reverse("Manish"));                 // [ 'h', 's', 'i', 'n', 'a', 'M' ]
+// console.log(Reverse(["PQR", "XYZ", "ABC"]));    // [ 'ABC', 'XYZ', 'PQR' ]
+// console.log(Reverse([10, 20, 30, 40]));         // [ 40, 30, 20, 10 ]
+// console.log(Reverse(10));                       // Compile time Error
+
+// -------------------------------------
+
 // Create a function Reverse which can accept string or array of any type and give the following output
+type StringOrArray = string | string[] | number[];
+type StringOrNumberArray = string[] | number[];
 
-function Reverse() {
+function Reverse(word: string): string[];
+function Reverse(words: string[]): string[];
+function Reverse(word: number[]): number[];
 
+function Reverse(strOrArr: StringOrArray): StringOrNumberArray {
+    if (typeof strOrArr === "string") {
+        return strOrArr.split("").reverse();
+    } else {
+        return strOrArr.reverse();
+    }
 }
 
 console.log(Reverse("Manish"));                 // [ 'h', 's', 'i', 'n', 'a', 'M' ]
 console.log(Reverse(["PQR", "XYZ", "ABC"]));    // [ 'ABC', 'XYZ', 'PQR' ]
 console.log(Reverse([10, 20, 30, 40]));         // [ 40, 30, 20, 10 ]
-console.log(Reverse(10));                       // Compile time Error
+// console.log(Reverse(10));                       // Compile time Error
