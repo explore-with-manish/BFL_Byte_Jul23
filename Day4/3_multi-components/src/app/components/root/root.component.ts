25723adc40e8e1d0b5b088abf299a45ed2aac913
manish.sharma <manish.sharma@technizerindia.com>
2023-07-27 17:20:29 +0530

    Day 4 Final

---
+++ Day4/3_multi-components/src/app/components/root/root.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styles: [
+  ]
+})
+export class RootComponent {
+
+}
