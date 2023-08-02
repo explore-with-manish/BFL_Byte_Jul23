9938a8e5922c8544f2cf18b655ff103ec27e7c48
manish.sharma <manish.sharma@technizerindia.com>
2023-08-02 15:42:04 +0530

    PWA App Added

--- Day9/2_pwa/src/app/app.module.ts
+++ Day9/2_pwa/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { APP_BOOTSTRAP_LISTENER, ComponentRef, NgModule } from '@angular/core';
+import { APP_BOOTSTRAP_LISTENER, ComponentRef, NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RootComponent } from './components/root/root.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmployeeModule } from './employee/employee.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,13 @@ import { EmployeeModule } from './employee/employee.module';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    EmployeeModule
+    EmployeeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
--- Day9/2_pwa/src/app/components/root/root.component.ts
+++ Day9/2_pwa/src/app/components/root/root.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './root.component.html',
-  styles: [
-  ]
+  templateUrl: './root.component.html'
 })
-export class RootComponent {
+export class RootComponent implements OnInit{
+  
+  isOnline: boolean = false;
+  ngOnInit(): void {
+    this.updateOnlineStatus();
+
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus():void {
+    this.isOnline = navigator.onLine;
+  }
 
 }
